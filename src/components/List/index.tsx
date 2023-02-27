d739492827ac8ed9a7ec3c1bfd45538cdcd6b9b9
David Lazar <me@davidlazar.co>
2023-02-27 20:19:28 +0000

    style: changed title color

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -17,7 +17,7 @@ const List = () => {
 
   return (
     <div className='m-8'>
-      <h2 className='text-2xl font-semibold leading-5'>Allowances</h2>
+      <h2 className='text-2xl font-semibold leading-5 text-green-400'>Allowances</h2>
       <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mt-7'>
         {allowances.map((allowance) => (
           <Card key={allowance.id} allowance={allowance}></Card>
